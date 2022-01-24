74e6ec3529b1611910fa3f901460e90ffdba79d6
Mikhail Volkov <volkov.work1@gmail.com>
2022-01-24 07:04:19 +0300

    add pdf to email, start making transaction

--- server/controllers/functions/getAllWears.js
+++ server/controllers/functions/getAllWears.js
@@ -1,6 +1,6 @@
 const Wear = require('../../models/Wear');
 
 module.exports = async (req, res) => {
-  const data = await Wear.find({ active: true });
+  const data = await Wear.find({ active: true, quantity: { $gt: 0 } });
   res.status(200).json(data);
 };
--- server/controllers/functions/postCheckout.js
+++ server/controllers/functions/postCheckout.js
@@ -1,10 +1,13 @@
 const Order = require('../../models/Order');
 const User = require('../../models/User');
 const { ObjectId } = require('mongoose').Types;
+const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+const pdf = require('html-pdf');
 
 module.exports = async (req, res) => {
   try {
+    //prepare
     const user = await User.findOne({ _id: ObjectId(req.user.id) });
     let transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
@@ -15,10 +18,11 @@ module.exports = async (req, res) => {
         pass: process.env.ADMIN_EMAIL_PASS,
       },
     });
+    //making html
     let htmlBody = '';
     htmlBody += `<b>Пользователь: ${user.name} ${user.lastname}</b><br/>`;
     htmlBody += `<b>Эл. почта: ${user.email}</b><br/><br/>`;
-    htmlBody += `<h1>\tPurchases:</h1></br><ol>`;
+    htmlBody += `<h1>\tТовары:</h1></br><ol>`;
     const data = req.body;
     const wears = [];
     let sum = 0;
@@ -31,21 +35,43 @@ module.exports = async (req, res) => {
     });
     htmlBody += '</ol><br/><hr/>';
     htmlBody += `<b><i>Итог = ${sum} ${data[0].currency}</b></i>`;
+    const session = await mongoose.startSession();
+    session.startTransaction();
     const order = new Order({ data: wears, userId: ObjectId(req.user.id), orderDate: Date.now() });
-    order.save();
-    await transporter.sendMail({
-      from: process.env.ADMIN_EMAIL,
-      to: process.env.ADMIN_EMAIL,
-      subject: 'check from "Clothes"',
-      text: htmlBody,
-      html: htmlBody,
-    });
-    await transporter.sendMail({
-      from: process.env.ADMIN_EMAIL,
-      to: user.email,
-      subject: 'check from "Clothes"',
-      text: htmlBody,
-      html: htmlBody,
+    await order.save();
+    //send
+    pdf.create(htmlBody).toBuffer(async function (err, buffer) {
+      if (err) {
+        throw err;
+      }
+      await transporter.sendMail({
+        from: process.env.ADMIN_EMAIL,
+        to: process.env.ADMIN_EMAIL,
+        subject: 'check from "Clothes"',
+        text: htmlBody,
+        html: htmlBody,
+        attachments: [
+          {
+            filename: `чек_${user.name}_${user.lastname}.pdf`,
+            content: buffer,
+            contentType: 'application/pdf',
+          },
+        ],
+      });
+      await transporter.sendMail({
+        from: process.env.ADMIN_EMAIL,
+        to: user.email,
+        subject: 'check from "Clothes"',
+        text: htmlBody,
+        html: htmlBody,
+        attachments: [
+          {
+            filename: `чек_${user.name}_${user.lastname}.pdf`,
+            content: buffer,
+            contentType: 'application/pdf',
+          },
+        ],
+      });
     });
     res.status(200).json({ message: 'Order created' });
   } catch (error) {
